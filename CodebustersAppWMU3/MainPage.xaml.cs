31f45d963e8c6814dff0876f96cfb4e7a9b42203
optimusprime <s143522@student.hb.se>
2017-03-08 11:29:55 +0100

    added compass

--- CodebustersAppWMU3/MainPage.xaml.cs
+++ CodebustersAppWMU3/MainPage.xaml.cs
@@ -1,19 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Sensors;
 using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+using Windows.UI.Core;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+// Required to access the core dispatcher object
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// Required to access the sensor platform and the compass
+
 
 namespace CodebustersAppWMU3
 {
@@ -22,9 +15,38 @@ namespace CodebustersAppWMU3
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private Compass _compass; // Our app' s compass object
+
+        // This event handler writes the current compass reading to
+        // the textblocks on the app' s main page.
+
+        private async void ReadingChanged(object sender, CompassReadingChangedEventArgs e)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                CompassReading reading = e.Reading;
+                txtMagnetic.Text = String.Format("{0,5:0.00}", reading.HeadingMagneticNorth);
+                if (reading.HeadingTrueNorth.HasValue)
+                    txtNorth.Text = String.Format("{0,5:0.00}", reading.HeadingTrueNorth);
+                else
+                    txtNorth.Text = "No reading.";
+            });
+        }
+
         public MainPage()
         {
             this.InitializeComponent();
+            _compass = Compass.GetDefault(); // Get the default compass object
+
+            // Assign an event handler for the compass reading-changed event
+            if (_compass != null)
+            {
+                // Establish the report interval for all scenarios
+                uint minReportInterval = _compass.MinimumReportInterval;
+                uint reportInterval = minReportInterval > 16 ? minReportInterval : 16;
+                _compass.ReportInterval = reportInterval;
+                _compass.ReadingChanged += new TypedEventHandler<Compass, CompassReadingChangedEventArgs>(ReadingChanged);
+            }
         }
     }
 }
