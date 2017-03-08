22eb6151d63a1f37acd5114ee6cfb083a3906cb5
optimusprime <s143522@student.hb.se>
2017-03-08 10:49:45 +0100

    1..2...3...GOO!

---
+++ CodebustersAppWMU3/Models/Room.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodebustersAppWMU3.Models
+{
+    class Room
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+        public double Volume { get; set; }   
+    }
+}
---
+++ CodebustersAppWMU3/Models/Surface.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace CodebustersAppWMU3.Models
+{
+    class Surface
+    {
+        public Image SurfaceImage { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public double Area { get; set; }
+    }
+}
